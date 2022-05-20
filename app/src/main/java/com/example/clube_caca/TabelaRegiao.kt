108c94ca71434c07ec4cb5a4d7418b9058c1ee71
1700948 <jaha5825@gmail.com>
2022-05-20 11:41:41 +0100

    Criada TabelaRegiao.kt e livre de erros
    Signed-off-by: 1700948 <jaha5825@gmail.com>

--- app/src/main/java/com/example/clube_caca/TabelaRegiao.kt
+++ app/src/main/java/com/example/clube_caca/TabelaRegiao.kt
@@ -1,5 +1,45 @@
 package com.example.clube_caca
 
-class TabelaRegiao(db: SQLiteDatabase) {
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
+class TabelaRegiao(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+    fun cria(){
+        db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $MEDIA_ANIMAIS INTEGER NOT NULL, $TAMANHO_ZONA FLOAT NOT NULL, $ANIMAIS_ZONA TEXT NOT NULL )")
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+    fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+    companion object {
+        const val NOME_TABELA = "Regiões"
+        const val CAMPO_NOME = "NomedaRegiao"
+        const val MEDIA_ANIMAIS = "MediaAnimais"
+        const val TAMANHO_ZONA = "TamanhoDaZona"
+        const val ANIMAIS_ZONA = "NumeroHabitantesNaoVacinados"
+
+
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME)
+    }
 }
